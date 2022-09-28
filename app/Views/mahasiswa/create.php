faa26d7daa908a2d5905b2e432eb942c8c4412ed
nursetiowati2013 <nur.setiowati2013@students.unila.ac.id>
2022-09-28 11:20:33 +0700

    Prak5

--- app/Views/mahasiswa/create.php
+++ app/Views/mahasiswa/create.php
@@ -3,41 +3,32 @@
 <div class="row">
     <div class="col-6">   
     <form action ="/store" method="POST">
-    <div class="p-4"> 
 
-    <form action="/store" method="POST">
+      <div class="form-group">
+        <label for="npm">NPM</label>
+        <input type="text" name="npm" class="form-control" id="npm" >
+      </div>
 
-  <div class="form-group">
-    <label for="npm">NPM</label>
-    <input type="text" name="npm" class="form-control" id="npm" >
-  </div>
-  <div class="form-group">
-    <label for="nama">Nama</label>
-    <input type="text" name="nama" class="form-control" id="nama">
-  </div>
-  <div class="form-group">
-    <label for="alamat">Alamat</label>
-    <input type="text" name="alamat" class="form-control" id="alamat">
-  </div>
-  <div class="form-group">
+      <div class="form-group">
+        <label for="nama">Nama</label>
+        <input type="text" name="nama" class="form-control" id="nama">
+      </div>
+
+      <div class="form-group">
+        <label for="alamat">Alamat</label>
+        <input type="text" name="alamat" class="form-control" id="alamat">
+      </div>
+
+      <div class="form-group">
             <label for="deskripsi">Deskripsi</label>
             <textarea type="text" name="deskripsi" class="form-control" id="deskripsi"></textarea>
-   </div>
+      </div>
+
   <button type="submit" class="btn btn-dark">Submit</button>
-</form>
-</div>
+    </form>
+  </div>
 </div>
 
 
 <?= $this->endSection(); ?>
-<div class="form-group">
-        <label for="nama">Nama</label>
-        <input type="text" name="nama" class="form-control" id="nama">
-    </div>
-    <div class="form-group">
-        <label for="alamat">Alamat</label>
-        <input type="text" name="alamat" class="form-control" id="alamat">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
+
--- app/Views/mahasiswa/edit.php
+++ app/Views/mahasiswa/edit.php
@@ -1,15 +1,36 @@
-<form action ="/update/<?= $id ?>" method="post">
-  <div class="form-group">
-    <label for="npm">NPM</label>
-    <input type="text" name="npm" class="form-control" id="npm" value = "<?=$npm?>" >
+<?= $this->extend('templates/template'); ?>
+<?= $this->section('content'); ?>
+<div class="p-4">
+    <div class="row">
+        <div class="col-6">
+
+          <form action ="/update/<?= $mahasiswa['id'] ?>" method="POST">
+            <div class="form-group">
+              <label for="npm">NPM</label>
+              <input type="text" name="npm" class="form-control" id="npm" value = "<?=$mahasiswa['npm'] ?>" >
+            </div>
+
+            <div class="form-group">
+              <label for="nama">Nama</label>
+              <input type="text" name="nama" class="form-control" id="nama" value = "<?=$mahasiswa ['nama'] ?>" >
+            </div>
+
+            <div class="form-group">
+              <label for="alamat">Alamat</label>
+              <input type="text" name="alamat" class="form-control" id="alamat" value = "<?=$mahasiswa['alamat']?>">
+            </div>
+
+            <div class="form-group">
+                    <label for="deskripsi">Deskripsi</label>
+                    <textarea type="text" name="deskripsi" class="form-control" id="deskripsi" value="<?= $mahasiswa['deskripsi'] ?>"> </textarea>
+            </div>
+
+            <button type="submit" class="btn btn-dark">Submit</button>
+      </form>
+      <div class="col-6">
+
+    </div>
   </div>
-  <div class="form-group">
-    <label for="nama">Nama</label>
-    <input type="text" name="nama" class="form-control" id="nama" value = "<?=$nama?>" >
-  </div>
-  <div class="form-group">
-    <label for="alamat">Alamat</label>
-    <input type="text" name="alamat" class="form-control" id="alamat" value = "<?=$alamat?>">
-  </div>
-  <button type="submit" class="btn btn-dark">Submit</button>
-</form>
+</div>
+
+<?= $this->endSection(); ?>
