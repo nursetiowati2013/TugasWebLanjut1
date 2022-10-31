978c015ec6bfacd869aaa1e3a7090859deeb6944
nursetiowati2013 <83541218+nursetiowati2013@users.noreply.github.com>
2022-10-31 13:53:57 +0700

    Update Routes.php

--- Routes.php
+++ Routes.php
@@ -5,6 +5,7 @@ namespace Config;
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
+
 if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
     require SYSTEMPATH . 'Config/Routes.php';
 }
@@ -16,16 +17,27 @@ $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Login::index');
+$routes->get('/home', 'Home');
+$routes->post('/admin', 'Login::admin');
 $routes->get('/mahasiswa', 'MahasiswaController::index');
 $routes->get('/create', 'MahasiswaController::create');
 $routes->post('/store', 'MahasiswaController::store');
+$routes->get('/keluar', 'Logout::logout');
 $routes->delete('/delete/(:num)', 'MahasiswaController::delete/$1');
 $routes->post('/update/(:num)', 'MahasiswaController::update/$1');
 $routes->get('/edit/(:num)', 'MahasiswaController::edit/$1');
-$routes->get('/index', 'Pages::index'); 
-$routes->get('(:any)', 'Pages::view/$1');
+$routes->get('/index', 'Pages::index');
+$routes->get('/ask', 'Pages::ask');
 
+// Admin
+$routes->get('/tambahProduk', 'Admin::index');
+$routes->post('/simpanProduk', 'Admin::simpan');
+
+$routes->get('/tambahPertanyaan', 'Admin::ask');
+$routes->post('/simpanPertanyaan', 'Admin::save');
+// General
+$routes->get('(:any)', 'Pages::view/$1');
 
 
 if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
